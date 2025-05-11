2338252b1423ecc671de8d3f2d57d4b2a33b7a73
Michael Setiawan <michaelsetiawan823@gmail.com>
2025-05-11 14:04:39 -0400

    Routing fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: (process.env.REACT_APP_API_URL || '') + '/api',
   headers: {
     'Content-Type': 'application/json',
   },
